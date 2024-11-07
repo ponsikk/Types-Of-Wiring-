2c6f2e3174dd60a7411286dc203a556a37a2d1d3
Ilya <ponsik98@mail.ru>
2024-11-07 12:47:24 +0300

    done

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,14 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+
+    @Override
+    public String toString() {
+        return "Это конец Кощея!";
+    }
 }
+
+
